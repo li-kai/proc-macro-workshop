3c3633e7d1b83f58a856e97d33a20d92fbf63a9d
Li Kai <hello+github@lik.ai>
2020-09-22 03:58:21 +0800

    Add repeated field behavior

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -12,7 +12,24 @@ macro_rules! span_compile_error {
     };
 }
 
-#[proc_macro_derive(Builder)]
+#[allow(dead_code)]
+struct ExpandToArg {
+    keyword: syn::Ident,
+    equal_token: syn::Token![=],
+    name: syn::LitStr,
+}
+
+impl syn::parse::Parse for ExpandToArg {
+    fn parse(input: syn::parse::ParseStream) -> Result<Self, syn::Error> {
+        Ok(ExpandToArg {
+            keyword: input.parse()?,
+            equal_token: input.parse()?,
+            name: input.parse()?,
+        })
+    }
+}
+
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
@@ -34,36 +51,85 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let optional_fields = fields.iter().map(|field| {
         let name = &field.ident;
-        let field_type = if let Some(nested_field_type) = get_optional_field_type(&field.ty) {
-            nested_field_type
-        } else {
-            &field.ty
-        };
+        let field_type = &field.ty;
+        let normalized_field_type = get_optional_field_type(field_type).unwrap_or(field_type);
 
-        quote! {
-            #name: std::option::Option<#field_type>
+        if get_vec_field_type(field_type).is_some() {
+            quote! {
+                #name: #field_type
+            }
+        } else {
+            quote! {
+                #name: std::option::Option<#normalized_field_type>
+            }
         }
     });
 
     let builder_values = fields.iter().map(|field| {
         let name = &field.ident;
-        quote! {
-            #name: None
+        let field_type = &field.ty;
+
+        if get_vec_field_type(field_type).is_some() {
+            quote! {
+                #name: std::vec::Vec::new()
+            }
+        } else {
+            quote! {
+                #name: None
+            }
         }
     });
 
     let builder_field_setters = fields.iter().map(|field| {
         let name = &field.ident;
-        let field_type = if let Some(nested_field_type) = get_optional_field_type(&field.ty) {
-            nested_field_type
-        } else {
-            &field.ty
-        };
+        let field_type = &field.ty;
 
-        quote! {
-            fn #name(&mut self, #name: #field_type) -> &mut Self {
-                self.#name = std::option::Option::Some(#name);
-                self
+        if let Some(vec_field_type) = get_vec_field_type(field_type) {
+            let field_setter_fn = quote! {
+                fn #name(&mut self, #name: #field_type) -> &mut Self {
+                    self.#name = #name;
+                    self
+                }
+            };
+
+            let each_attr = match field
+                .attrs
+                .iter()
+                .find(|attr| attr.path.is_ident("builder"))
+            {
+                Some(each_attr) => each_attr,
+                None => return field_setter_fn,
+            };
+            let each_attr_name = match each_attr.parse_args::<ExpandToArg>() {
+                Ok(expand) if expand.keyword == "each" => {
+                    format_ident!("{}", expand.name.value().trim_matches('"'))
+                }
+                _ => return field_setter_fn,
+            };
+
+            let each_field_setter_fn = quote! {
+                fn #each_attr_name(&mut self, #each_attr_name: #vec_field_type) -> &mut Self {
+                    self.#name.push(#each_attr_name);
+                    self
+                }
+            };
+
+            if &Some(each_attr_name) == name {
+                each_field_setter_fn
+            } else {
+                quote! {
+                    #field_setter_fn
+                    #each_field_setter_fn
+                }
+            }
+        } else {
+            let normalized_field_type = get_optional_field_type(field_type).unwrap_or(field_type);
+
+            quote! {
+                fn #name(&mut self, #name: #normalized_field_type) -> &mut Self {
+                    self.#name = std::option::Option::Some(#name);
+                    self
+                }
             }
         }
     });
@@ -75,14 +141,16 @@ pub fn derive(input: TokenStream) -> TokenStream {
             .expect("named fields do not have identifier");
         let error_msg = format!("field \"{}\" is None", name);
 
-        if let Some(_) = get_optional_field_type(&field.ty) {
-            // as None is an acceptable value, we do not unwrap the Option
-            return quote! {
+        if get_optional_field_type(&field.ty).is_some() || get_vec_field_type(&field.ty).is_some() {
+            // for optional fields, None is an acceptable value, we do not unwrap the Option
+            // for vector fields, their default is an empty vector, so we just clone it
+            quote! {
                 #name: self.#name.clone()
-            };
-        }
-        quote! {
-            #name: self.#name.clone().ok_or(#error_msg)?
+            }
+        } else {
+            quote! {
+                #name: self.#name.clone().ok_or(#error_msg)?
+            }
         }
     });
 
@@ -132,6 +200,36 @@ fn get_optional_field_type(field_type: &syn::Type) -> Option<&syn::Type> {
     let path_segment = &path_segments[0];
 
     if path_segment.ident != "Option" {
+        return None;
+    }
+    let args = match &path_segment.arguments {
+        syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+            colon2_token: None,
+            lt_token: _,
+            gt_token: _,
+            args,
+        }) => args,
+        _ => return None,
+    };
+
+    if let Some(syn::GenericArgument::Type(sub_field_type)) = args.first() {
+        Some(sub_field_type)
+    } else {
+        None
+    }
+}
+
+fn get_vec_field_type(field_type: &syn::Type) -> Option<&syn::Type> {
+    let path_segments = match field_type {
+        syn::Type::Path(syn::TypePath { qself: _, path }) => &path.segments,
+        _ => return None,
+    };
+    if path_segments.is_empty() {
+        return None;
+    }
+    let path_segment = &path_segments[0];
+
+    if path_segment.ident != "Vec" {
         return None;
     }
     let args = match &path_segment.arguments {
